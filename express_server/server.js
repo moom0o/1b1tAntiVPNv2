5c8741e94b67a5fcfb6b04f7d518ae5d91d8bb1e
moom0o <48740106+moom0o@users.noreply.github.com>
2020-06-04 15:33:38 -0400

    Update server.js

--- express_server/server.js
+++ express_server/server.js
@@ -1,15 +1,26 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const fs = require("fs");
 const app = express();
 let array = [];
 let proxycheck = {}
+// our default array of dreams
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 app.use(express.urlencoded());
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 app.get("/proxycheck", (request, response) => {
-  if(request.query.key != "yyyyyyyyyyy"){
+  console.log(request.headers)
+  console.log(request.url)
+  if(request.query.key != "key"){
     return response.status(401).send("Wrong token")
   }
   if(proxycheck[request.query.ip]){
@@ -35,12 +46,13 @@ app.get("/proxycheck", (request, response) => {
     })
   }
   var unirest = require('unirest');
-  var req = unirest('GET', `https://proxycheck.io/v2/${request.query.ip}?vpn=1&asn=1&key=xxxxx-xxxxx-xxxxx-xxxxx`)
+  var req = unirest('GET', `https://proxycheck.io/v2/${request.query.ip}?vpn=1&asn=1&key=key`)
     .headers({
-      'Cookie': '__cfduid=xxxxxxxxxxxxxxxxxxxxxxxxxxxxx; __cflb=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+      'Cookie': 'cookie'
     })
     .end(function (res) { 
       if (res.error)
+      console.log(res.raw_body);
       response.send({
         proxy: `${JSON.parse(res.raw_body)[request.query.ip].proxy}`,
         cached: "no",
@@ -52,10 +64,11 @@ app.get("/proxycheck", (request, response) => {
 var multer = require("multer");
 var upload = multer();
 app.post("/addname", upload.none(), (request, response) => {
+  console.log(request.body);
   var unirest = require("unirest");
   var req = unirest(
     "POST",
-    `https://www.google.com/recaptcha/api/siteverify?secret=realsecret&response=${
+    `https://www.google.com/recaptcha/api/siteverify?secret=secret&response=${
       request.body["g-recaptcha-response"]
     }`
   ).end(function(res) {
@@ -66,16 +79,18 @@ app.post("/addname", upload.none(), (request, response) => {
       var req = unirest('GET', `https://api.mojang.com/users/profiles/minecraft/${request.body["username"].replace("(", "").replace(")", "")}`)
         .end(function (res) { 
           if (res.error){
+            console.log(res.error)
             response.status(400).send("Invalid username")
             return
           } else{
+            console.log("no error")
             if(!res.raw_body.includes("id")){
               response.status(400).send("Invalid username")
               return
             } else {
               let names = JSON.parse(fs.readFileSync(`/home/1b1t/1b1twhitelist/names.json`, "utf-8"));
               array = names;
-              array.push(request.body["username"].replace("(", "").replace(")", ""));
+              array.push(request.body["username"].replace("(", "").replace(")", "").toLowerCase());
               fs.writeFileSync(`/home/1b1t/1b1twhitelist/names.json`, JSON.stringify(array, null, 4), err => {
                 if (err) throw err;
               });
@@ -85,10 +100,22 @@ app.post("/addname", upload.none(), (request, response) => {
             }
           }
         });
+
+        
+
+
     } else {
       response.status(403).send("Google Recaptcha failed!");
     }
   });
+});
+
+app.get("/names", (request, response) => {
+  let names = JSON.parse(fs.readFileSync(`/home/1b1t/1b1twhitelist/names.json`, "utf-8"));
+  response.send(["no", "fuck", "you", "kek", "this", "isn't", "even", "used", "anymore"]);
+});
+
+// listen for requests :)
 const listener = app.listen(3002, () => {
   console.log("Your app is listening on port " + 3002);
 });
